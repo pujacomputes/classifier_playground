c89eaf1b3144d4c2826e4e13b9bb4b9b6445b5e6
Puja <pujat@umich.edu>
2022-08-05 13:26:49 -0700

    Adding VAT support

--- train_vat_clip.py
+++ train_vat_clip.py
@@ -1,5 +1,11 @@
+from collections import OrderedDict
+import copy
 import os
 import time
+
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from clip_model import ClipModel
+from vat_loss import VATLoss
 import torch
 import timm
 import tqdm 
@@ -10,15 +16,121 @@ import numpy as np
 import torch.nn.functional as F
 from utils import *
 import pdb 
-from clip_model import ClipModel
-from torch.utils.data.distributed import DistributedSampler
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-"""
-Wrapped into function so we 
-can call it for fine-tuning too.
-"""
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 PREFIX="/p/lustre1/trivedi1/compnets/classifier_playground/"
+def extract_features(args, model,loader,train_aug,train_transform):
+    if train_aug in ['cutmix','mixup','cutout']:
+        transform = train_transform
+    else:
+        transform = None
+    features = []
+    labels = []
+    model.eval()
+    with torch.no_grad():
+        for data, targets in tqdm.tqdm(loader,disable=True):
+            data = data.cuda()
+            if transform:
+                if train_aug in ['cutmix','mixup']:
+                    images, targets= transform(images,target=targets)
+                if train_aug == 'cutout':
+                    images = transform(images)
+            if args.arch == 'resnet50':
+                #using a timm model. called the 
+                reps = torch.nn.functional.adaptive_avg_pool2d(model.module.forward_features(data),1)
+                features.append(reps.detach().cpu().numpy())
+                labels.append(targets.detach().cpu().numpy())
+            if "clip" in args.arch:
+                #using a timm model. called the
+                reps = model.module.get_features(data)
+                features.append(reps.detach().cpu().numpy())
+                labels.append(targets.detach().cpu().numpy())
+    # features = torch.nn.functional.adaptive_avg_pool2d(np.squeeze(np.concatenate(features)),1)
+    features = np.squeeze(np.concatenate(features))
+    labels = np.concatenate(labels)
+    return features, labels
+
+def linear_probe_vat(args, net, train_loader,test_loader, train_aug, train_transform):
+    net.eval()
+    train_features, train_labels = extract_features(args,model=net, loader=train_loader,train_aug=train_aug,train_transform=train_transform)
+    test_features, test_labels = extract_features(args,model=net, loader=test_loader,train_aug='test',train_transform=None)
+    print(train_features.shape)
+
+    rep_train_dataset = torch.utils.data.TensorDataset(torch.Tensor(train_features),torch.Tensor(train_labels).long())
+    rep_train_dataloader = torch.utils.data.DataLoader(rep_train_dataset,batch_size=args.batch_size,shuffle=True) 
+    
+    rep_test_dataset = torch.utils.data.TensorDataset(torch.Tensor(test_features),torch.Tensor(test_labels).long())
+    rep_test_dataloader = torch.utils.data.DataLoader(rep_test_dataset,batch_size=args.batch_size,shuffle=True) 
+    
+    """
+    Create a linear probe layer.
+    We will attach it separately back.
+    """
+    
+    fc = torch.nn.Linear(train_features.shape[1],NUM_CLASSES_DICT[args.dataset]).to(DEVICE)
+    optimizer = torch.optim.SGD(
+        fc.parameters(),
+        args.learning_rate,
+        momentum=args.momentum,
+        weight_decay=args.decay,
+        nesterov=True)
+
+    scheduler = LR_Scheduler(
+        optimizer,
+        warmup_epochs=0, warmup_lr = 0*args.batch_size/256, 
+        num_epochs=args.epochs, base_lr=args.learning_rate*args.batch_size/256, 
+        final_lr =1e-5 *args.batch_size/256, 
+        iter_per_epoch= len(train_loader),
+        constant_predictor_lr=False
+    )
+    ALPHA = args.alpha
+    criterion = torch.nn.CrossEntropyLoss()
+    vat_loss = VATLoss(xi=10.0, eps=1.0, ip=1)
+    for epochs in range(args.epochs):
+        vat_avg = 0
+        consistency_loss_avg = 0
+        loss_avg = 0
+        for batch_idx, (data, target) in enumerate(rep_train_dataloader):
+            data, target = data.to(DEVICE), target.to(DEVICE)
+            optimizer.zero_grad()
+
+            # LDS should be calculated before the forward for cross entropy
+            lds = vat_loss(fc, data)
+            output = fc(data)
+            consistency_loss = criterion(output, target) 
+            loss = consistency_loss + ALPHA * lds
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            vat_avg += ALPHA * lds
+            consistency_loss_avg += consistency_loss
+            loss_avg += loss
+        _,train_acc = test(fc, rep_train_dataloader) 
+        _,val_acc = test(fc, rep_test_dataloader) 
+        
+        vat_avg /= len(rep_train_dataloader) 
+        consistency_loss_avg /= len(rep_train_dataloader) 
+        loss_avg /= len(rep_train_dataloader) 
+        print("Epoch: {4} -- VAT: {0:.4f} -- Con: {1:.4f} -- Tot.:{2:.4f} -- Train Acc: {3:.4f} -- Test Acc: {5:.4f}".format(vat_avg,consistency_loss_avg,loss_avg,train_acc,epochs,val_acc))
+    #Set the classifier weights
+    net.module.fc = fc 
+   
+    """
+    Compute acc. using forward passes.
+    """
+    _,acc = test(net=net,test_loader=test_loader)
+    print("Completed VAT Training: {0:.3f}".format(acc))
+    checkpoint = {
+        'lp_epoch': args.epochs,
+        'dataset': args.dataset,
+        'model': args.arch,
+        'state_dict': net.state_dict(),
+        'best_acc': acc,
+        'protocol':'vatlp'
+    }
+    return net, checkpoint
+
 def train_loop(args,protocol,save_name,log_path, net, optimizer,scheduler,start_epoch,end_epoch,train_loader, test_loader, train_aug, train_transform):
 
     use_clip_mean = "clip" in args.arch
@@ -120,7 +232,17 @@ def train_loop(args,protocol,save_name,log_path, net, optimizer,scheduler,start_
         'optimizer': optimizer.state_dict(),
         'protocol':args.protocol
     }
-    return net,checkpoint                              
+    return net,checkpoint                                               
+
+def load_dp_ckpt(model,pretrained_ckpt):
+    ckpt = torch.load(pretrained_ckpt)
+    new_keys = [(k, "module.{}".format(k)) for k in list(ckpt['state_dict'].keys())]
+    for old_k, new_k in new_keys:
+        ckpt['state_dict'][new_k] = ckpt['state_dict'].pop(old_k)
+    incompatible, unexpected = model.load_state_dict(ckpt['state_dict'],strict=False)
+    print("Incompatible Keys: ", incompatible)
+    print("Unexpected Keys: ",unexpected)
+    return model
 
 def main():
     args = arg_parser()
@@ -128,14 +250,13 @@ def main():
         print("=> " ,arg, getattr(args, arg))
     print("=> Device: ",DEVICE)
     print("=> Num GPUS: ",torch.cuda.device_count())
-
+    
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
     if not os.path.exists(args.save):
         os.makedirs(args.save)
     if not os.path.isdir(args.save):
         raise Exception('%s is not a dir' % args.save)
-
 
     if args.pretrained_ckpt == "None":
         if args.arch  == 'ResNet50':
@@ -156,7 +277,7 @@ def main():
             print("\t => Using Default Clip Ckpt!!") 
             net = ClipModel(model_name=encoder_type,scratch=False)
     use_clip_mean = "clip" in args.arch 
-
+ 
     save_name =  args.dataset \
         + '_' + args.arch \
         + '_'+ args.protocol \
@@ -169,7 +290,8 @@ def main():
         + "_" + str(args.ft_learning_rate) \
         + "_" + str(args.ft_decay) \
         + "_" + str(args.l2sp_weight) \
-        + "_" + str(args.seed)
+        + "_" + str(args.seed) \
+        + "_" + str(args.alpha) #term specific for VAT to adjust reguarlization weight
     
     print("******************************")
     print(save_name)
@@ -183,6 +305,7 @@ def main():
     print('Reset Classifer: ',net.get_classifier()) 
     # Distribute model across all visible GPUs
     net = torch.nn.DataParallel(net).cuda()
+    net = net.cuda()
     torch.backends.cudnn.benchmark = True
 
     """
@@ -190,7 +313,7 @@ def main():
     """
     lp_train_acc, lp_test_acc, lp_train_loss, lp_test_loss = -1,-1,-1,-1
     ft_train_acc, ft_test_acc, ft_train_loss, ft_test_loss = -1,-1,-1,-1
-    if args.protocol in ['lp','lp+ft']:
+    if args.protocol in ['vatlp','vatlp+ft']:
 
         log_path = os.path.join("{}/logs".format(PREFIX),
                             "lp+" + save_name + '_training_log.csv')
@@ -211,20 +334,20 @@ def main():
         print("=> Train: ",train_loader.dataset) 
         print("=> Test: ",test_loader.dataset) 
 
-        test_loss, test_acc = test(net, test_loader)
-        print("=> Epoch 0 Test Acc: ",test_acc)
-
         """
-        Passing only the fc layer to the optimizer. 
+        Passing only the fc layer to optimizer 
         This prevents lower layers from being effected by weight decay.
         """
-        if args.resume_lp_ckpt.lower() != "none" and args.protocol == 'lp+ft':
+        if args.resume_lp_ckpt.lower() != "none" and args.protocol == 'vatlp+ft':
             print("****************************")
-            print("Loading Saved LP Ckpt")
+            print("Loading Saved vatlp Ckpt")
             ckpt = torch.load(args.resume_lp_ckpt)
-            incomp, unexpected = net.load_state_dict(ckpt['state_dict'])
-            print("Incompatible Keys: ",incomp)
-            print("Unexpected Keys: ",unexpected)
+            try:
+                incomp, unexpected = net.load_state_dict(ckpt['state_dict'])
+                print("Incompatible Keys: ",incomp)
+                print("Unexpected Keys: ",unexpected)
+            except:
+                net =load_dp_ckpt(net, args.resume_lp_ckpt)
 
             lp_train_loss, lp_train_acc = test(net, train_loader)
             lp_test_loss, lp_test_acc = test(net, test_loader)
@@ -238,52 +361,16 @@ def main():
             print("****************************")
             print("=> Freezing Layers!")
             net = freeze_layers_for_lp(net)
-            optimizer = torch.optim.SGD(
-                net.module.fc.parameters(),
-                args.learning_rate,
-                momentum=args.momentum,
-                weight_decay=args.decay,
-                nesterov=True)
-
-            start_epoch = 0
-            # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            #     optimizer,
-            #     T_max = args.epochs,
-            #     eta_min = 1e-5,
-            # )
-
-            scheduler = LR_Scheduler(
-                optimizer,
-                warmup_epochs=0, warmup_lr = 0*args.batch_size/256, 
-                num_epochs=args.epochs, base_lr=args.learning_rate*args.batch_size/256, 
-                final_lr =1e-5 *args.batch_size/256, 
-                iter_per_epoch= len(train_loader),
-                constant_predictor_lr=False
-            )
-            with open(log_path, 'w') as f:
-                f.write('epoch,time(s),train_loss,test_loss,test_error(%)\n')
 
             """
             Perform Linear Probe Training 
             """
-            net, ckpt = train_loop(args = args,
-                protocol='lp',
-                save_name = "lp+"+save_name,
-                log_path=log_path,
-                net = net, 
-                optimizer = optimizer,
-                scheduler = scheduler,
-                start_epoch = start_epoch,
-                end_epoch = args.epochs,
-                train_loader = train_loader, 
-                test_loader = test_loader, 
-                train_aug = args.train_aug, 
-                train_transform=train_transform)
+            net, ckpt = linear_probe_vat(args, net, train_loader, test_loader, args.train_aug, train_transform)
 
             """
             Save LP Final Ckpt.
             """
-            s = "lp+{save_name}_final_checkpoint_{epoch:03d}_pth.tar".format(save_name=save_name,epoch=args.epochs)
+            s = "vatlp+{save_name}_final_checkpoint_{epoch:03d}_pth.tar".format(save_name=save_name,epoch=args.epochs)
             save_path = os.path.join(args.save, s)
             torch.save(ckpt, save_path)
 
@@ -293,7 +380,7 @@ def main():
     """
     Performing Fine-tuing Training!
     """
-    if args.protocol in ['lp+ft','ft','lpfrz+ft']:
+    if args.protocol in ['vatlp+ft','ft']:
         if args.protocol == 'lpfrz+ft':
             print("=> Freezing Classifier, Unfreezing All Other Layers!")
             net = unfreeze_layers_for_lpfrz_ft(net)
@@ -305,7 +392,7 @@ def main():
         """
         Select FT Augmentation Scheme.
         """
-        if args.protocol == 'lp+ft' and args.resume_lp_ckpt.lower() == 'none':
+        if args.protocol == 'vatlp+ft' and args.resume_lp_ckpt.lower() == 'none':
             del train_loader, test_loader, optimizer, scheduler, train_transform, test_transform
         ft_train_transform = get_transform(dataset=args.dataset, SELECTED_AUG=args.ft_train_aug,use_clip_mean=use_clip_mean) 
         ft_test_transform = get_transform(dataset=args.dataset, SELECTED_AUG=args.ft_test_aug,use_clip_mean=use_clip_mean) 
@@ -314,11 +401,9 @@ def main():
             test_aug=args.ft_test_aug, 
             train_transform=ft_train_transform,
             test_transform=ft_test_transform,
+            use_ft=True,
             use_clip_mean=use_clip_mean) 
 
-        test_loss, test_acc = test(net, ft_test_loader)
-        print("=> Epoch 0 Test Acc: ",test_acc)
-        
         optimizer = torch.optim.SGD(
             net.parameters(),
             args.ft_learning_rate,
